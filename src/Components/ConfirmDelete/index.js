368acbdc1624fbc63296bfc9ee1ac6d0f801d025
Nguyen Nhat Phap <nguyennhatphapbp@gmail.com>
2024-05-07 08:37:33 +0700

    fix bug confirm delete

--- src/Components/ConfirmDelete/index.js
+++ src/Components/ConfirmDelete/index.js
@@ -1,91 +1,93 @@
-import Dialog from "@mui/material/Dialog";
-import DialogActions from "@mui/material/DialogActions";
-import DialogContent from "@mui/material/DialogContent";
-import DialogContentText from "@mui/material/DialogContentText";
-import DialogTitle from "@mui/material/DialogTitle";
-import Button from "@mui/material/Button";
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Button from '@mui/material/Button';
 import {
-  delete_Course,
-  delete_Student,
-  delete_Teacher,
-  get_Student,
-  get_Teacher,
-  get_courses,
-} from "~/services/API_DBS";
+    delete_Course,
+    delete_Student,
+    delete_Teacher,
+    get_Student,
+    get_Teacher,
+    get_courses,
+} from '~/services/API_DBS';
 
 export default function ConfirmDelete({
-  displayConfirm,
-  setDisplayConfirm,
-  rowToAction,
-  student,
-  setDataStu,
-  teacher,
-  setDataTea,
-  course,
-  setDataCourse,
+    displayConfirm,
+    setDisplayConfirm,
+    rowToAction,
+    student,
+    setDataStu,
+    teacher,
+    setDataTea,
+    course,
+    setDataCourse,
 }) {
-  const handleClose = () => {
-    setDisplayConfirm(false);
-  };
-  const handleDel = () => {
-    if (student) {
-      delete_Student(rowToAction)
-        // .then((data) => {
-        //   setDataStu(data["data"] ?? []);
-        // })
-        .then(() => {
-          get_Student().then((data) => {
-            setDataStu(data["data"] ?? []);
-          });
-        })
-        .catch((error) => {
-          console.error("Delete failed:", error);
-          // Xử lý lỗi xảy ra khi thực hiện yêu cầu DELETE
-        });
-    } else if (teacher) {
-      delete_Teacher(rowToAction)
-        .then(() => {
-          get_Teacher().then((data) => {
-            setDataTea(data["data"] ?? []);
-          });
-        })
-        .catch((error) => {
-          console.error("Delete failed: ", error);
-        });
-    } else if (course) {
-      delete_Course(rowToAction)
-        .then(() => {
-          get_courses().then((data) => {
-            setDataCourse(data["data"] ?? []);
-          });
-        })
-        .catch((error) => {
-          console.error("Delete failed: ", error);
-        });
-    }
+    const handleClose = () => {
+        setDisplayConfirm(false);
+    };
+    const handleDel = () => {
+        if (student) {
+            delete_Student(rowToAction)
+                // .then((data) => {
+                //   setDataStu(data["data"] ?? []);
+                // })
+                .then(() => {
+                    get_Student().then((data) => {
+                        setDataStu(data['data'] ?? []);
+                    });
+                })
+                .catch((error) => {
+                    console.error('Delete failed:', error);
+                    // Xử lý lỗi xảy ra khi thực hiện yêu cầu DELETE
+                });
+        } else if (teacher) {
+            delete_Teacher(rowToAction)
+                .then(() => {
+                    get_Teacher().then((data) => {
+                        setDataTea(data['data'] ?? []);
+                    });
+                })
+                .catch((error) => {
+                    console.error('Delete failed: ', error);
+                });
+        } else if (course) {
+            delete_Course(rowToAction)
+                .then(() => {
+                    get_courses({ searchFlag: true }).then((data) => {
+                        setDataCourse(data['data'] ?? []);
+                    });
+                })
+                .catch((error) => {
+                    console.error('Delete failed: ', error);
+                });
+        }
 
-    handleClose();
-  };
+        handleClose();
+    };
 
-  return (
-    <Dialog
-      open={displayConfirm}
-      onClose={handleClose}
-      aria-labelledby="alert-dialog-title"
-      aria-describedby="alert-dialog-description"
-    >
-      <DialogTitle id="alert-dialog-title">{"Xoá dữ liệu này ?"}</DialogTitle>
-      <DialogContent>
-        <DialogContentText id="alert-dialog-description">
-          Dữ liệu khi xóa không thể khôi phục
-        </DialogContentText>
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={handleClose}>Không</Button>
-        <Button onClick={handleDel} autoFocus>
-          Đồng ý
-        </Button>
-      </DialogActions>
-    </Dialog>
-  );
+    return (
+        <Dialog
+            open={displayConfirm}
+            onClose={handleClose}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+        >
+            <DialogTitle id="alert-dialog-title">
+                {'Xoá dữ liệu này ?'}
+            </DialogTitle>
+            <DialogContent>
+                <DialogContentText id="alert-dialog-description">
+                    Dữ liệu khi xóa không thể khôi phục
+                </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={handleClose}>Không</Button>
+                <Button onClick={handleDel} autoFocus>
+                    Đồng ý
+                </Button>
+            </DialogActions>
+        </Dialog>
+    );
 }
